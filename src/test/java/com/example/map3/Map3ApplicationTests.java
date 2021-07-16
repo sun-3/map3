09c42b26c6f1ef518338f473e88b478edda55463
sun-3 <72216343+sun-3@users.noreply.github.com>
2021-07-16 14:27:10 +0530

    Initial commit

---
+++ src/test/java/com/example/map3/Map3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.map3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Map3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
